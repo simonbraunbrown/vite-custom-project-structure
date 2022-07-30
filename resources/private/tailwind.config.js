cfa7628d2279ded231ff19cc6a22e740fc275f28
simonbraunbrown <sijo.braun@googlemail.com>
2022-07-30 18:09:45 +0200

    add dasyUI

--- resources/private/tailwind.config.js
+++ resources/private/tailwind.config.js
@@ -6,5 +6,15 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["light", "dark"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
--- resources/private/templating/src/js/main.js
+++ resources/private/templating/src/js/main.js
@@ -7,3 +7,12 @@ document.querySelector('#app').innerHTML = `
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   <img src="${imgUrl}" alt="vite logo">
 `;
+
+const themeToggle = document.querySelector('.theme-toggle');
+themeToggle.addEventListener('change', () => {
+  if (themeToggle.checked) {
+    document.querySelector('html').setAttribute('data-theme','dark');
+  } else {
+    document.querySelector('html').setAttribute('data-theme','light');
+  }
+})
